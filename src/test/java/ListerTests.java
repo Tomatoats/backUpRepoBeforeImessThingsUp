3e6fce5db6505f525c0e84511e0ae02395e62549
Alexys Veloz <alexys.veloz.fl@gmail.com>
2021-11-07 16:43:13 -0500

    made the starter tests

--- src/test/java/ListerTests.java
+++ src/test/java/ListerTests.java
@@ -31,8 +31,56 @@ public class ListerTests {
         item1[2] = "Make sure to buy dad's gift before hand! Address: 123 Sesame Street";
         item1[3] = "incomplete";
         List1.add(item1);
+    }
+    @Test
+    void CanitHandle100(){
 
     }
+    @Test
+    void MakeItem(){
+
+    }
+    @Test
+    void DescriptionLengthError(){
+
+    }
+    @Test
+    void DueDateOptionality(){
+
+    }
+    @Test
+    void ValidDates(){
+
+    }
+    @Test
+    void ProperDateFormat(){
+
+    }
+    @Test
+    void removeItem(){
+
+    }
+    @Test
+    void clearList(){
+
+    }
+    @Test
+    void EditDescription(){
+
+    }
+    @Test
+    void EditdueDate(){
+
+    }
+    @Test
+    void MarkItemComplete(){
+
+    }
+    @Test
+    void MarkItemIncomplete(){
+
+    }
+
     @Test
     void DesplayAll(){
         //in this one, once display all is clicked, we'll call
@@ -51,96 +99,11 @@ public class ListerTests {
         // element being "incomplete" is shown.
     }
     @Test
-    void editTheListName(){
-        //since the list name is always the first element of the arrayList, we call
-        //it and change Listname[0].
-
-    }
-
-    @Test
-    void editTheDueDate(){
-        //We figure out which item they want to switch the name, call that specific
-        //item in the arrayList, and then change whatever item that is by making it's
-        //1st element to whatever string the user puts
-        //if it doesn't fit the YYYY-MM-DD set, ask the user to try again.
-    }
-    @Test
-    void editTheDescription(){
-        //We figure out which item they want to switch the name, call that specific
-        //item in the arrayList, and then change whatever item that is by making it's
-        //2nd element to whatever string the user puts
-    }
-    @Test
-    void LoadError(){
-        //We try to load in a txt Lister file, and if it isn't in the
-        //proper lister format, we send in an error. We purposely load in
-        //a wrong file and make sure that we get an error
+    void saveTheList(){
 
     }
     @Test
-    void LoadingInFile(){
-        //we try to take in a proper lister File, and I make sure that
-        //we can extract the info from the file properly and have valid info
+    void chooseSaveNameAndLocation(){
     }
-    @Test
-    void turnComplete(){
-        //we find the item that got turned complete and turn it's 3rd element
-        //string to "complete"
-    }
-    @Test
-    void turnIncomplete(){
-        //we find the item that got turned incomplete and turn it's 3rd element
-        //string to "incomplete"
-    }
-
-    @Test
-    void CheckSetDueDate(){
-        //we have a string, we check to make sure it is in proper format,
-        //and we set some item[1] to said string.
-        //double check with an assert that it properly took it.
-
-    }
-    @Test
-    void CheckSetDescription(){
-        //we have a string, and we set some item[3] to said string,
-        //double check with an assert that it properly took it
-        //String can be empty in this one btw
-    }
-
-    @Test
-    void DoesDeleteItemWork(){
-        //we're gonna remove an item from the ArrayList, and double check it worked
-        //by checking Arraylist Size.
-    }
-    @Test
-    void DoesClearListWork(){
-        //we call on the CLearList class and call it's deleteAll function.
-    }
-    @Test
-    void DoesStartSceneWork(){
-        //we call deleteList, and then make sure StartScene opens afterward so the user
-        //can choose to load or start a new list.
-    }
-    @Test
-    void GetStringsForWarning(){
-        //we have warning have a string thats empty, and we set that string to
-        //whatever String we get from another class, and then show that to the user.
-        //make sure the warning matches what we actually want it to say.
-    }
-    @Test
-    void checkGetArrayList(){
-        //We make sure that when we use the getArrayList function, it properly gives
-        //us the arrayList we need and we can get info from it.
-    }
-    @Test
-    void checkGetItem(){
-        //we make sure that when we use the getItem function, it porperly gives us
-        //the Item we need and we can get info from it
-    }
-
-
-
-
-
 
 }
